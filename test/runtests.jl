4a638f6146d9aa395f35a60cb37d9614304f1ea8
johti <johti17@liu.se>
2019-07-18 16:19:43 +0200

    More dep updates

---
+++ test/runtests.jl
@@ -0,0 +1,11 @@
+using Test
+
+@testset "Compiling Absyn via wildcard" begin
+  using Absyn
+  @show(PROGRAM)
+end
+
+@testset "Test that we have Absyn.Program"
+  import Absyn
+  @show(Program)
+end
