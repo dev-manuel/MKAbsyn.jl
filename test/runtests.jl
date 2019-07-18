340b92fee9488cdcb926db616d5d9de3f7005f5c
johti <johti17@liu.se>
2019-07-18 17:29:15 +0200

    ud t

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,11 @@ using Absyn
 using Test
 
 @testset "Compiling Absyn via wildcard" begin
-  @show(PROGRAM)
+  @test_nowarn @show(PROGRAM)
 end
 
 @testset "Test that we have Absyn.Program"
-  @show(Program)
+@test_nowarn @show(Program)
 end
 
 end
